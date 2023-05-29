15c8b30d5e1f3424b6183078d72368c81ed56a83
Avellow <avellow@yandex.ru>
2023-05-29 10:39:24 +0300

    initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import classes from './Counter.module.scss';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counter {count}</h1>
+      <button onClick={increment} className={classes.btn}>increment</button>
+    </div>
+  )
+}
